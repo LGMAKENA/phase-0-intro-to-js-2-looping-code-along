f23c0e072aa3b8d2544ad393fcf9c8ee505aabe9
lgmakena <linetmakenag@gmail.com>
2023-03-25 13:07:01 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
+
+  const gifts = ["teddy bear", "drone", "doll"];
+
+  function wrapGifts(gifts){
+      for(let i = 0;i<gifts.length;i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      debugger;
+      }
+    }
+    wrapGifts(gifts);
+
+    function writeCards(names,events){
+        for(i = 0;i<names.length;i++)
+        console.log(`Thank you $(names[i]), for the wonderful $(event)gift`);
+          }
+        writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+        function countDown(number){
+for(i = parseInt(number);i=0;i--){//takes a number and checks if equal to zero or greater and decreases 
+console.log(i);// prints the current number
+        }}
